6770e0d8bd4164f0fe0608cfa81c26b670256e72
Gokhan Ergin ERYILDIR <ergineryildir@gmail.com>
2024-04-28 08:19:17 +0300

    Update DragAndDrop .cs

--- Copilot/DragAndDrop .cs
+++ Copilot/DragAndDrop .cs
@@ -1,1 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+    private Color originalColor;
+
+    void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+
+        // Save the original color and change the color of the object to red when picked up
+        originalColor = GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+
+    void OnMouseUp()
+    {
+        // Restore the original color when the object is put down
+        GetComponent<Renderer>().material.color = originalColor;
+
+        // Check if we are over a box or a shelf
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+
+            // If the object we hit is a box or a shelf, parent this object to it
+            if (objectHit.CompareTag("Box") || objectHit.CompareTag("Shelf"))
+            {
+                this.transform.parent = objectHit;
+            }
+        }
+    }
+}
 
